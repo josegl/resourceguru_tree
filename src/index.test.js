4548b86ce1aec36f177d5561971d1f79783b11db
Jose G.L <jgl@mailbox.org>
2022-08-08 18:53:45 +0200

    Create test that match the assertions from the main entry point

--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,36 @@
 const Node = require('./Node');
+let tree;
+
+beforeAll(() => {
+  tree = new Node(
+    "÷",
+    null,
+    new Node(
+      "+",
+      null,
+      new Node("", 7, null, null),
+      new Node(
+        "x",
+        null,
+        new Node("-", null, new Node("", 3, null, null), new Node("", 2, null, null)),
+        new Node("", 5, null, null)
+      )
+    ),
+    new Node("", 6, null, null)
+  );
+  return 0;
+})
 
 describe('Refactor', () => {
   test('const node = new Node(), node instanceof Node === true', () => {
     const node = new Node();
-    expect(node instanceof Node).toBe(true);
+    expect(tree instanceof Node).toBe(true);
+  });
+  test('toString tree function call result matches the given expression string', () => {
+    expect(tree.toString()).toEqual("((7 + ((3 - 2) x 5)) ÷ 6)");
+  });
+  test('resul tree function call matches the expected result', () => {
+    expect(tree.result()).toBe(2);
   });
 });
 
